4c7d9e2d392d14298fbd7ebb09098170d9074bd4
Takku <farelvivo06@gmail.com>
2024-04-08 02:57:38 +0700

    added query builder join

--- database.sql
+++ database.sql
@@ -23,3 +23,16 @@ values ('sample', 0);
 
 select *
 From counters;
+
+create table products
+(
+    id          varchar(100) not null primary key,
+    name        varchar(100) not null,
+    description text         null,
+    price       int          not null,
+    category_id varchar(100) not null,
+    created_at  timestamp    not null default current_timestamp,
+    constraint fk_category_id foreign key (category_id) references categories (id)
+) engine innodb;
+
+select * from products;
